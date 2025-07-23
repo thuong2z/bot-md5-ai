c7867e3dcc09ec5369ba305bc6fa0de7fe0b2911
thuong2z <bysff05@gmail.com>
2025-07-23 18:57:07 +0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+TOKEN = "8014506210:AAGt8NMOsVBVfDuRQcUAzJNHLt0BYaSzpjI"  # Thay bằng token @BotFather
+
+ADMIN_ID = [7434281447]  # ID Telegram admin (danh sách)
+
+BANK_INFO = {
+    "stk": "9704229201867027150",
+    "bank": "MB Bank",
+    "name": "BUI TIEN GIAP"
+}
+
+RATE = 2000  # 2000đ = 1 xu
+MIN_NAP = 20000
+MAX_NAP = 1000000
